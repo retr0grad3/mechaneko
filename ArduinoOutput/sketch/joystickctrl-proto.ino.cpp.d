a05dc2a82e642af362b2b65d4a4f3b77ecbc7858
kebin <kevi1806@hotmail.com>
2024-03-16 22:50:12 -0700

    Configured Arduino VSC integration

---
+++ ArduinoOutput/sketch/joystickctrl-proto.ino.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\kebin\Documents\GitHub\mechaneko\ArduinoOutput\sketch\joystickctrl-proto.ino.cpp.o: \
+ C:\Users\kebin\Documents\GitHub\mechaneko\ArduinoOutput\sketch\joystickctrl-proto.ino.cpp \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WCharacter.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/HardwareSerial.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBAPI.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\kebin\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\mega/pins_arduino.h
