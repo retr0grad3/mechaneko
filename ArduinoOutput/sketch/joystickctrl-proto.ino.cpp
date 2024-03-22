634d8503cc3616cb4aa8c26d56fe7fcd3a6afff1
kebin <kevi1806@hotmail.com>
2024-03-21 19:33:22 -0700

    FUCKED AROUND

--- ArduinoOutput/sketch/joystickctrl-proto.ino.cpp
+++ ArduinoOutput/sketch/joystickctrl-proto.ino.cpp
@@ -1,14 +1,12 @@
-# 1 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
+#include <Arduino.h>
+#line 1 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
 /* 
-
-
 
 Makes a group of motors move given a joystick's input
 
-
-
 */
-# 8 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
+
+
 // Motor 1 (X axis) pins
 int in1 = 42;
 int in2 = 44;
@@ -24,22 +22,27 @@ int dirUp = 53;
 int dirRight = 49;
 int dirLeft = 47;
 
+#line 23 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
+void setup();
+#line 45 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
+void loop();
+#line 23 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
 void setup(){
-
+    
     // Motor 1 pin modes
-    pinMode(enA, 0x1);
-    pinMode(in1, 0x1);
-    pinMode(in2, 0x1);
+    pinMode(enA, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
     // Motor 2 pin modes
-    pinMode(enB, 0x1);
-    pinMode(in3, 0x1);
-    pinMode(in4, 0x1);
+    pinMode(enB, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
 
     // Joystick pin modes
-    pinMode(dirDown, 0x2);
-    pinMode(dirUp, 0x2);
-    pinMode(dirLeft, 0x2);
-    pinMode(dirRight, 0x2);
+    pinMode(dirDown, INPUT_PULLUP);
+    pinMode(dirUp, INPUT_PULLUP);
+    pinMode(dirLeft, INPUT_PULLUP);
+    pinMode(dirRight, INPUT_PULLUP);
 
     // Set up serial output
     Serial.begin(9600);
@@ -47,128 +50,88 @@ void setup(){
 }
 
 void loop(){
-
+  
     // Motor 1 (in1, in2, enA) = X axis
     // Motor 2 (in3, in4, enB) = Y axis
 
-    if(digitalRead(dirUp) == 0x0){
-        digitalWrite(in1, 0x1); // MOTOR 1 FORWARD ON
-        digitalWrite(in2, 0x0); // REVERSE OFF
+    if(digitalRead(dirUp) == LOW){
+        digitalWrite(in1, HIGH); // MOTOR 1 FORWARD ON
+        digitalWrite(in2, LOW); // REVERSE OFF
         digitalWrite(enA, 255); // SPEED MAX
-        digitalWrite(in3, 0x0); // MOTOR 2 FORWARD OFF (must be opposite)
-        digitalWrite(in4, 0x1); // REVERSE OFF
+        digitalWrite(in3, LOW); // MOTOR 2 FORWARD OFF (must be opposite)
+        digitalWrite(in4, HIGH); // REVERSE OFF
         digitalWrite(enB, 255); // SPEED MAX
         Serial.println("DOWN");
 
-    } else if(digitalRead(dirDown) == 0x0){
-        digitalWrite(in1, 0x0); // MOTOR 1 FORWARD OFF
-        digitalWrite(in2, 0x1); // REVERSE ON
+    } else if(digitalRead(dirDown) == LOW){
+        digitalWrite(in1, LOW); // MOTOR 1 FORWARD OFF
+        digitalWrite(in2, HIGH); // REVERSE ON
         digitalWrite(enA, 255); // SPEED MAX
-        digitalWrite(in3, 0x1); // MOTOR 2 FORWARD ON
-        digitalWrite(in4, 0x0); // REVERSE OFF
+        digitalWrite(in3, HIGH); // MOTOR 2 FORWARD ON
+        digitalWrite(in4, LOW); // REVERSE OFF
         digitalWrite(enB, 255); // SPEED MAX
         Serial.println("UP");
 
-    } else if(digitalRead(dirLeft) == 0x0){
+    } else if(digitalRead(dirLeft) == LOW){
         Serial.println("LEFT");
 
-    } else if(digitalRead(dirRight) == 0x0){
+    } else if(digitalRead(dirRight) == LOW){
         Serial.println("RIGHT");
 
     } else {
-        digitalWrite(in1, 0x0); // MOTOR 1/X FORWARD OFF
-        digitalWrite(in2, 0x0); // REVERSE OFF
+        digitalWrite(in1, LOW); // MOTOR 1/X FORWARD OFF
+        digitalWrite(in2, LOW); // REVERSE OFF
         digitalWrite(enA, 0); // SPEED MIN
 
-        digitalWrite(in3, 0x0); // MOTOR 2/Y FORWARD OFF
-        digitalWrite(in4, 0x0); // REVERSE OFF
+        digitalWrite(in3, LOW); // MOTOR 2/Y FORWARD OFF
+        digitalWrite(in4, LOW); // REVERSE OFF
         digitalWrite(enB, 0); // SPEED MIN
 
         Serial.println("CENTER");
         delay(100);
-
+        
     }
 
     /*
 
-
-
     if(digitalRead(dirUp) == LOW){
-
         digitalWrite(in1, HIGH); // MOTOR 1/X FORWARD ON
-
         digitalWrite(in2, LOW); // REVERSE OFF
-
         digitalWrite(enA, 255); // SPEED MAX
-
         Serial.println("DOWN");
 
-
-
     } else if(digitalRead(dirDown) == LOW){
-
         digitalWrite(in1, LOW); // MOTOR 1/X FORWARD OFF
-
         digitalWrite(in2, HIGH); // REVERSE ON
-
         digitalWrite(enA, 255); // SPEED MAX
-
         Serial.println("UP");
 
-
-
     } else if(digitalRead(dirLeft) == LOW){
-
         digitalWrite(in3, HIGH); // MOTOR 2/Y FORWARD ON
-
         digitalWrite(in4, LOW); // REVERSE OFF
-
         digitalWrite(enB, 255); // SPEED MAX
-
         Serial.println("LEFT");
 
-
-
     } else if(digitalRead(dirRight) == LOW){
-
         digitalWrite(in3, LOW); // MOTOR 2/Y FORWARD OFF
-
         digitalWrite(in4, HIGH); // REVERSE ON
-
         digitalWrite(enB, 255); // SPEED MAX
-
         Serial.println("RIGHT");
 
-
-
     } else {
-
         digitalWrite(in1, LOW); // MOTOR 1/X FORWARD OFF
-
         digitalWrite(in2, LOW); // REVERSE OFF
-
         digitalWrite(enA, 0); // SPEED MIN
 
-
-
         digitalWrite(in3, LOW); // MOTOR 2/Y FORWARD OFF
-
         digitalWrite(in4, LOW); // REVERSE OFF
-
         digitalWrite(enB, 0); // SPEED MIN
 
-
-
         Serial.println("CENTER");
-
         delay(100);
-
         
-
     }
 
-
-
     */
-# 130 "C:\\Users\\kebin\\Documents\\GitHub\\mechaneko\\prototyping\\joystickctrl-proto\\joystickctrl-proto.ino"
+
 }
